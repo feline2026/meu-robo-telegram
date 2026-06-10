942296cf8a99d991b882760a30cabe90838c9179
feline2026 <tarcisofelline@gmail.com>
2026-06-10 18:54:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ from telegram.ext import (
 )
 
 # coloque o seu token do telegram entre as aspas abaixo
-TOKEN = "SEU_TELEGRAM_TOKEN_AQUI"
+TOKEN = "71829382:AAHjg..." 
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Olá! Envie o nome de um produto para buscar.")
