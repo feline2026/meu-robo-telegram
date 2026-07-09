917cc46cf95849b4e5428c53248e6a47fed590c5
feline2026 <tarcisofelline@gmail.com>
2026-07-09 15:22:25 -0300

    Update autos.py

--- autos.py
+++ autos.py
@@ -16,9 +16,10 @@ class VisualSiteHandler(BaseHTTPRequestHandler):
         self.send_response(200); self.send_header('Content-type', 'text/html; charset=utf-8'); self.end_headers()
         prod_texto = ""; html_botoes = ""; texto_resultados = "<h2>StockNegócio - Buscador Automotivo Ativo!</h2>"
         query_params = urllib.parse.parse_qs(urllib.parse.urlparse(self.path).query)
-        produto = query_params.get('p', [''])
-        if produto and produto:
+        produto = query_params.get('p', [''])[0]
+        if produto:
             prod_texto = produto.strip()
+
             t_olx = urllib.parse.quote_plus(prod_texto)
             t_wm = urllib.parse.quote_plus(prod_texto.lower().replace(" ", "-"))
             t_ml = urllib.parse.quote_plus(prod_texto)
